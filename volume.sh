c418e2557f6897e05b9b5945ee2105f73a3e5f5e
Nikolaj Esberg <nikolajesberg@gmail.com>
2026-09-24 10:53:00 +0200

    hd

--- volume.sh
+++ volume.sh
@@ -3,4 +3,5 @@ set -e
 docker volume create data
 docker run --rm -v data:/data alpine sh -c "echo Hej fra volumen! > /data/besked.txt"
 docker run --rm -v data:/data alpine cat /data/besked.txt
-docker run --rm alpine cat /data/besked.txt
+if ! docker run --rm alpine cat /data/besked.txt; then
+echo "virker" fi
