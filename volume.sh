1d034ec98731497996ae8d1fb7cb2516df38e092
Nikolaj Esberg <nikolajesberg@gmail.com>
2026-09-24 10:57:48 +0200

    dkdk

--- volume.sh
+++ volume.sh
@@ -7,3 +7,7 @@ docker run --rm -v data:/data alpine cat /data/besked.txt
 if ! docker run --rm alpine cat /data/besked.txt; then
   echo "Som forventet: uden volumen kan containeren ikke se filen"
 fi
+
+docker run --rm --name skriver -v data:/data mit-image2 sh -c "echo Besked fra mit eget image! > /data/mitimage.txt"
+docker rm -f skriver 2>/dev/null || true
+docker run --rm -v data:/data alpine cat /data/mitimage.txt
