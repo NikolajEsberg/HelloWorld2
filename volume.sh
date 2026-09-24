bc69ff5c862a35c873a9121a6b7f1a21daec85b0
Nikolaj Esberg <nikolajesberg@gmail.com>
2026-09-24 10:50:06 +0200

    udvidet

--- volume.sh
+++ volume.sh
@@ -3,3 +3,4 @@ set -e
 docker volume create data
 docker run --rm -v data:/data alpine sh -c "echo Hej fra volumen! > /data/besked.txt"
 docker run --rm -v data:/data alpine cat /data/besked.txt
+docker run --rm alpine cat /data/besked.txt
