8a30a1cadb54f27e4a36a350f94f96db54154112
Nikolaj Esberg <nikolajesberg@gmail.com>
2026-09-24 10:55:08 +0200

    kf

--- volume.sh
+++ volume.sh
@@ -3,5 +3,7 @@ set -e
 docker volume create data
 docker run --rm -v data:/data alpine sh -c "echo Hej fra volumen! > /data/besked.txt"
 docker run --rm -v data:/data alpine cat /data/besked.txt
+
 if ! docker run --rm alpine cat /data/besked.txt; then
-echo "virker" fi
+  echo "Som forventet: uden volumen kan containeren ikke se filen"
+fi
