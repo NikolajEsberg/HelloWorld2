93abb82bffe9173cc3c0b5edbda5145d54ffa3cd
Nikolaj Esberg <nikolajesberg@gmail.com>
2026-09-24 10:00:08 +0200

    stoppelse og fjernelse af containere

--- container.sh
+++ container.sh
@@ -2,5 +2,8 @@ set -e
 docker run -d --name min-container nginx
 docker run -d --name min-anden-container nginx
 docker exec  min-container ls /usr/share/nginx/html
-docker logs min-container 
-
+docker logs min-container
+docker stop min-container
+docker stop min-anden-container
+docker rm min-container
+docker rn min-anden-container
